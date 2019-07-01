94a5980c2a4284729e15a8b3a5dd701b576011f0
Subhajit Sahu <wolfram77@gmail.com>
2019-07-01 22:27:24 +0530

    p_mqtt: use optimized nvs open, close

--- main/p_mqtt.c
+++ main/p_mqtt.c
@@ -16,8 +16,10 @@ static uint32_t interval = 0;
 
 esp_err_t mqtt_get_config_json(char *buf) {
   size_t length;
-  NVS_READ(nvs, nvs_get_str(nvs, MQTT_URI_KEY, uri, &length));
-  NVS_READ(nvs, nvs_get_u32(nvs, MQTT_INTERVAL_KEY, &interval));
+  NVS_OPEN(nvs);
+  ERET( nvs_get_str(nvs, MQTT_URI_KEY, uri, &length) );
+  ERET( nvs_get_u32(nvs, MQTT_INTERVAL_KEY, &interval) );
+  NVS_CLOSE(nvs);
   sprintf(buf, "{\"uri\": \"%s\", \"interval\": \"%d\"}", uri, interval);
   return ESP_OK;
 }
@@ -29,8 +31,10 @@ esp_err_t mqtt_set_config_json(esp_mqtt_client_handle_t handle, const char *json
   json_string(json, "\"uri\":", uri);
   printf("@ MQTT set config: uri=%s, interval=%d\n", uri, interval);
   ERET( esp_mqtt_client_set_uri(handle, uri) );
-  NVS_WRITE(nvs, nvs_set_u32(nvs, MQTT_INTERVAL_KEY, interval));
-  NVS_WRITE(nvs, nvs_set_str(nvs, MQTT_URI_KEY, uri));
+  NVS_OPENW(nvs);
+  ERET( nvs_set_u32(nvs, MQTT_INTERVAL_KEY, interval) );
+  ERET( nvs_set_str(nvs, MQTT_URI_KEY, uri) );
+  NVS_CLOSEW(nvs);
   return ESP_OK;
 }
 
@@ -38,16 +42,18 @@ esp_err_t mqtt_set_config_json(esp_mqtt_client_handle_t handle, const char *json
 esp_err_t mqtt_init(esp_mqtt_client_handle_t *handle) {
   printf("# Init MQTT client\n");
   size_t length = sizeof(uri);
-  NVS_READ(nvs, nvs_get_str(nvs, MQTT_URI_KEY, uri, &length));
+  NVS_OPENW(nvs);
+  ERET( nvs_get_str(nvs, MQTT_URI_KEY, uri, &length) );
   if (strlen(uri) == 0) {
     strcpy(uri, CONFIG_MQTT_BROKER_URL);
-    NVS_WRITE(nvs, nvs_set_str(nvs, MQTT_URI_KEY, uri));
+    ERET( nvs_set_str(nvs, MQTT_URI_KEY, uri) );
   }
-  NVS_READ(nvs, nvs_get_u32(nvs, MQTT_INTERVAL_KEY, &interval));
+  ERET( nvs_get_u32(nvs, MQTT_INTERVAL_KEY, &interval) );
   if (interval == 0) {
     interval = CONFIG_MQTT_PUBLISH_INTERVAL;
-    NVS_WRITE(nvs, nvs_set_u32(nvs, MQTT_INTERVAL_KEY, interval));
+    ERET( nvs_set_u32(nvs, MQTT_INTERVAL_KEY, interval) );
   }
+  NVS_CLOSEW(nvs);
   printf(": uri=%s, interval=%d\n", uri, interval);
   esp_mqtt_client_config_t c = {
     .uri = uri
