0922a3514e516bd5291d70df5dcc0e95f9d1473a
Subhajit Sahu <wolfram77@gmail.com>
2019-07-01 14:53:39 +0530

    p_wifi: use wifi_get_config_json, wifi_set_config_json

--- main/p_wifi.c
+++ main/p_wifi.c
@@ -1,42 +1,29 @@
 #include <string.h>
+#include <stdio.h>
 #include <esp_wifi.h>
-#include <esp_event.h>
 #include "p_json.h"
 #include "p_wifi.h"
 #include "macros.h"
 
 
-#define WIFI_AP_SSID      "charmender"
-#define WIFI_AP_PASSWORD  "charmender"
-
-
-esp_err_t wifi_config_sta_json(char *buff) {
+esp_err_t wifi_get_config_json(wifi_interface_t interface, char *buf) {
   wifi_config_t c;
-  ERET( esp_wifi_get_config(WIFI_IF_STA, &c) );
-  sprintf(buff,
-    "{\"ssid\": \"%s\", \"password\": \"%s\"}",
-    c.sta.ssid, c.sta.password);
+  ERET( esp_wifi_get_config(interface, &c) );
+  sprintf(buf, "{\"ssid\": \"%s\", \"password\": \"%s\"}",
+    interface == WIFI_IF_AP? c.ap.ssid : c.sta.ssid,
+    interface == WIFI_IF_AP? c.ap.password : c.ap.password);
   return ESP_OK;
 }
 
 
-esp_err_t wifi_set_config_sta_json(const char *json) {
+esp_err_t wifi_set_config_json(wifi_interface_t interface, const char *json) {
   wifi_config_t c;
-  ERET( esp_wifi_get_config(WIFI_IF_STA, &c) );
-  json_string(json, "\"ssid\":", (char*)c.sta.ssid);
-  json_string(json, "\"password\":", (char*)c.sta.password);
-  return esp_wifi_set_config(WIFI_IF_STA, &c);
-}
-
-
-void wifi_on_sta(void *arg, esp_event_base_t base, int32_t id, void *data) {
-  if (id == WIFI_EVENT_AP_STACONNECTED) {
-    wifi_event_ap_staconnected_t *d = (wifi_event_ap_staconnected_t*) data;
-    printf("- Station " MACSTR " joined, AID = %d (event)\n", MAC2STR(d->mac), d->aid);
-  } else if (id == WIFI_EVENT_AP_STADISCONNECTED) {
-    wifi_event_ap_stadisconnected_t *d = (wifi_event_ap_stadisconnected_t*) data;
-    printf("- Station " MACSTR " left, AID = %d (event)\n", MAC2STR(d->mac), d->aid);
-  }
+  ERET( esp_wifi_get_config(interface, &c) );
+  json_string(json, "\"ssid\":",
+    (char*) (interface == WIFI_IF_AP? c.ap.ssid : c.sta.ssid) );
+  json_string(json, "\"password\":",
+    (char*) (interface == WIFI_IF_AP? c.ap.password : c.sta.password));
+  return esp_wifi_set_config(interface, &c);
 }
 
 
--- main/p_wifi.h
+++ main/p_wifi.h
@@ -1,10 +1,14 @@
 #pragma once
-#include <esp_event.h>
+#include <esp_wifi.h>
 
 
-esp_err_t wifi_config_sta_json(char *buff);
-esp_err_t wifi_set_config_sta_json(const char *json);
-void wifi_on_sta(void *arg, esp_event_base_t base, int32_t id, void *data);
-esp_err_t wifi_start_apsta();
-esp_err_t wifi_start_sta();
+// Kconfig constants:
+// CONFIG_WIFI_AP_SSID
+// CONFIG_WIFI_AP_PASSWORD
+// CONFIG_WIFI_STA_SSID
+// CONFIG_WIFI_STA_PASSWORD
+
+
+esp_err_t wifi_get_config_json(wifi_interface_t interface, char *buf);
+esp_err_t wifi_set_config_json(wifi_interface_t interface, const char *json);
 esp_err_t wifi_init(const char *mac);
