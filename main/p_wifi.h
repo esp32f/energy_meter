cd5ef1dadaaa7899df4a1a7d7916e23746afbc71
Subhajit Sahu <wolfram77@gmail.com>
2019-07-01 14:27:59 +0530

    p_wifi: init now uses kconfig

--- main/p_wifi.h
+++ main/p_wifi.h
@@ -7,4 +7,4 @@ esp_err_t wifi_set_config_sta_json(const char *json);
 void wifi_on_sta(void *arg, esp_event_base_t base, int32_t id, void *data);
 esp_err_t wifi_start_apsta();
 esp_err_t wifi_start_sta();
-esp_err_t wifi_init();
+esp_err_t wifi_init(const char *mac);
